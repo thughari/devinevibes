9b3dc0359548739d0613412679b1b77a27e63f19
Hari <haribabutatikonda3@gmail.com>
2026-03-28 23:12:00 +0530

    initial commit

---
+++ src/main/java/com/devinevibes/DevinevibesApplication.java
@@ -0,0 +1,13 @@
+package com.devinevibes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevinevibesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevinevibesApplication.class, args);
+	}
+
+}
