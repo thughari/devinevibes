c7c4fc325073282e44bfd17c329147e524b23a06
Hari <haribabutatikonda3@gmail.com>
2026-04-04 23:10:09 +0530

    let's go

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://devinevibes-backend-83197747764.asia-south1.run.app/api',
-  googleClientId: ''
+  googleClientId: '83197747764-i59a82qh3ec4c39c14q6s0r3gfu4rjdj.apps.googleusercontent.com'
 };
