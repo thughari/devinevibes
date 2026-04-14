8a79f6ed1d700d5aa0a7b06153209906920dd449
thughari <thughari@users.noreply.github.com>
2026-04-14 19:35:41 +0000

    deploy: 24904ba2f98f10d31109242fc61590da7fc6575c

---
+++ chunk-4GC36OJ5.js
@@ -0,0 +1,1 @@
+var e={production:!0,apiUrl:"https://devinevibes-backend-83197747764.asia-south1.run.app/api",googleClientId:"83197747764-i59a82qh3ec4c39c14q6s0r3gfu4rjdj.apps.googleusercontent.com"};export{e as a};
